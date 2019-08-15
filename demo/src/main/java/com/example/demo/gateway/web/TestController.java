1b5d524827d86a77b293c71a5212f725755f4c71
lionking0105 <alexanderfoster0105@gmail.com>
2019-08-15 08:46:04 -0300

    comentando elk e efk, apenas graylog como default

--- demo/src/main/java/com/example/demo/gateway/web/TestController.java
+++ demo/src/main/java/com/example/demo/gateway/web/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
-import net.logstash.logback.argument.StructuredArguments;
 
 @Slf4j
 @RestController
@@ -13,19 +12,19 @@ public class TestController implements ErrorController {
 
   @GetMapping(path = "/info")
   public String infoCall() {
-    log.info("info call made", StructuredArguments.value("infoParam", "InfoParam1"));
+    log.info("info call made");
     return "Info";
   }
 
   @GetMapping(path = "/warn")
   public String warnCall() {
-    log.warn("warn call made", StructuredArguments.value("warnParam", "WarnParam1"));
+    log.warn("warn call made");
     return "Warn";
   }
 
   @GetMapping(path = "/error")
   public String errorCall() {
-    log.error("error call made", StructuredArguments.value("errorParam", "ErrorParam1"));
+    log.error("error call made");
     return "Error";
   }
 
